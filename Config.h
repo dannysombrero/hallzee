53f0ffe82e5ea56236c9cd5ed4a1455fb8b22615
dannysombrero <36120197+dannysombrero@users.noreply.github.com>
2026-09-01 00:26:33 -0400

    Define configurable student ID limits

--- Config.h
+++ Config.h
@@ -7,7 +7,9 @@ constexpr char CLOCK_CODE[] = "1234";
 constexpr char LOG_SUMMARY_CODE[] = "9999";
 constexpr char BLUETOOTH_DEVICE_NAME[] = "Hallzee";
 constexpr int MAX_BLUETOOTH_COMMAND_LENGTH = 48;
-constexpr int MAX_ID_LENGTH = 10;
+constexpr uint8_t MIN_STUDENT_ID_LENGTH = 4;
+constexpr uint8_t DEFAULT_STUDENT_ID_LENGTH = 10;
+constexpr uint8_t MAX_STUDENT_ID_LENGTH = 16;
 constexpr unsigned long RESET_HOLD_MS = 2000;
 
 // Display palette
